252a4081dc353a4745f06e4c39fcfaa0b96c4dea
Alexander Saratow <alexander.saratow@4plus.de>
2018-01-17 17:18:24 +0100

    added TBB configuration and some hints; using only the last project to push, because everything is stuffed in the same packages

--- nuget.cmd
+++ nuget.cmd
@@ -17,10 +17,15 @@ IF %MISSING_PARAMS% == 0 (
 	)
 )
 
+IF "%TBB_ROOT%" == "" (
+	ECHO Path to TBB root needs to be specified in TBB_ROOT.
+	ECHO You can set it using 'SET TBB_ROOT=...'.
+	SET MISSING_PARAMS=1
+)
+
 IF %MISSING_PARAMS% == 1 (
 	GOTO :MISSING_PARAMS
 )
-
 
 :CONFIGURE
 SET VTK_SRC=%CD%
@@ -35,6 +40,20 @@ CALL :CONFIGURE_PROJECT "Visual Studio 15 2017" vs15
 CALL :BUILD_PROJECT vs15
 
 POPD
+
+ECHO Final hints:
+ECHO vtkglew package probably contains only x64 binaries for any reason.
+ECHO You may need to open one of the solutions manually in Visual Studio and rebuild the nuget-push-vtkglew project to fix this.
+ECHO Also the vtkCommonCore package needs the TBB includes, which have to be copied manually from [tbb]/include to the
+ECHO NuGet/obj/vtkCommonCore/build/native/include/ directory for now.
+ECHO You will also have to copy TBB binaries from [tbb]/bin/... to according directories in
+ECHO [vtk-build]/NuGet/obj/vtkCommonCore/build/native/lib/... and adjust Custom_vtkCommonCore.targets files.
+ECHO Also note that you have to adjust [vs-config]/Common/Core/Custom_vtkCommonCore_[Configuration].targets file of the
+ECHO project you will use to push the NuGet package to the repository, because the one targets file in the NuGet directory
+ECHO would be overwritten during the build.
+ECHO A more elegant solution would imply the creation of a TBB NuGet package and the addition of a reference to it from the
+ECHO vtkCommonCore NuGet package.
+
 GOTO :EOF
 
 
@@ -58,10 +77,10 @@ GOTO :EOF
 
 
 :BUILD_PROJECT
-CALL :BUILD %1-x64-gl2	pack
-CALL :BUILD %1-x86-gl2	push
+CALL :BUILD %1-x86-gl	pack
 CALL :BUILD %1-x64-gl	pack
-CALL :BUILD %1-x86-gl	push
+CALL :BUILD %1-x86-gl2	pack
+CALL :BUILD %1-x64-gl2	push
 GOTO :EOF
 
 
@@ -103,6 +122,7 @@ cmake ^
 	-D NUGET_SOURCE=%NUGET_SOURCE% ^
 	-D NUGET_SUFFIX=%4 ^
 	-D VTK_RENDERING_BACKEND:STRING=%3 ^
+	-D VTK_SMP_IMPLEMENTATION_TYPE=TBB ^
 	-G "%~1" ^
 	%VTK_SRC%
 CD ..
